33dc6a2a26e3acb8e4d474e6541bcfa67f0d623f
jasonjsnell <snell.jason@gmail>
2017-03-08 17:30:49 -0800

    Initial Commit

---
+++ XvUtils/XvUtils.h
@@ -0,0 +1,19 @@
+//
+//  XvUtils.h
+//  XvUtils
+//
+//  Created by Jason Snell on 3/8/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvUtils.
+FOUNDATION_EXPORT double XvUtilsVersionNumber;
+
+//! Project version string for XvUtils.
+FOUNDATION_EXPORT const unsigned char XvUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvUtils/PublicHeader.h>
+
+
